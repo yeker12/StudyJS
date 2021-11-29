8ec746e582cacf8a1fbfcbd5deb470b3a05511c2
yeker <yeker12@naver.com>
2021-11-29 20:00:51 +0900

    Add and update  some function
    Learn how to connect CSS code in JavaScript

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
-const title = document.querySelector("div.testclass:first-child h1");
+const h1 = document.querySelector("div.testclass:first-child h1");
 
 function handleTilteClick(){
-    title.style.color = "blue";
+    const clickedClass = "clicked"
+   if(h1.className === "clicked"){
+       h1.className = "";
+   }
+   else{
+       h1.className = "clicked";
+   }
 }
 
 function handleMouseEnter(){
-    title.innerText = "Mouse is here!";
+    h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave(){
-    title.innerText = "Mouse is gone!!";
+    h1.innerText = "Mouse is gone!!";
 }
 
-title.addEventListener("click", handleTilteClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copy!!");
+}
+
+function handleWindowOffline(){
+    alert("SOS on WIFI!!");
+}
+
+function handleWindowOnline(){
+    alert("All Good!!!");
+}
 
 
+
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("click", handleTilteClick);
+h1.addEventListener("mouseleave", handleMouseLeave);
  
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy",handleWindowCopy);
+window.addEventListener("offline",handleWindowOffline);
+window.addEventListener("online",handleWindowOnline);
