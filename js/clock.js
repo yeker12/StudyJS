83da9698ef7aa38bb53806d7331954cfa8ebf9e1
yeker <yeker12@naver.com>
2021-12-03 19:52:46 +0900

    completed to make a clock

--- js/clock.js
+++ js/clock.js
@@ -1,2 +1,12 @@
-const clock = document.querySelector("h2#clcok");
+const clock = document.querySelector("h2#clock");
 
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock(); // 실행 후 바로 측정, 1초 후 뜨는 것을 방지
+setInterval(getClock, 1000);
