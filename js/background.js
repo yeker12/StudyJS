e42e6148dbcabed64ccef135d9a8ee07f5c03d09
yeker <yeker12@naver.com>
2021-12-03 21:14:47 +0900

    made random background image

---
+++ js/background.js
@@ -0,0 +1,11 @@
+ const images = [
+     "1.png","2.png","3.png","4.png","5.png",
+     "6.png","7.png","8.png","9.png","10.png",
+ ];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+
+bgImage.src = `image/${chosenImage}`;
+
+document.body.appendChild(bgImage);
