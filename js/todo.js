214b72d24a4dae90af7cab34a49f8d78d6125873
yeker <yeker12@naver.com>
2021-12-04 15:37:44 +0900

    Create function of Deleting to do list

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.querySelector("#todo-form");
 const toDOInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-List");
 
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newToDo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
+    const button = document.createElement("button");
     span.innerText = newToDo;
+    button.innerText = "❌"
+    button.addEventListener("click",deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
