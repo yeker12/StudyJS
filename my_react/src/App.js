6cd013216919ef90f69571bb224a81fe49fdc7b1
yeker <yeker12@naver.com>
2021-12-20 20:36:56 +0900

    Updated App.js Updated App.module.css Updated button.js Updated Button.module.css

--- my_react/src/App.js
+++ my_react/src/App.js
@@ -1,7 +1,11 @@
+import Button from "./button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>welcome back!!!!</h1>
+      <h1 className={styles.title}>welcome back!!!!</h1>
+      <Button text={"continue"} />
     </div>
   );
 }
