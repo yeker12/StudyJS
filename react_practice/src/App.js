001632a2d29b1a7f0919c717037cac6c9521c7ed
yeker <yeker12@naver.com>
2021-12-30 12:49:12 +0900

    Updated react.practice/src/App.js -Review To Do List using create-react-App

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,61 +1,30 @@
 import React, { useEffect, useState } from "react";
 
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [myMoney, setMymoney] = useState("");
-  const [money, setMoney] = useState(0);
-  const [value, setValue] = useState("");
-  const onChange = (event) => setMymoney(event.target.value);
-  const onSelectChange = (event) => setValue(event.target.value)
+  const [todo, setToDo] = useState("");
+  const [todos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if(myMoney === 0){
+    if(todo === "") {
       return ;
     }
-    setMoney((currentMoney) => ((myMoney === "") ? null :  parseInt(myMoney)));
-    setMymoney("");
-  }
-  useEffect(() => {
-    console.log("API");
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);})
-    }, [])
-
+    setToDos((currentArray) => [todo, ...currentArray]);
+    setToDo("");
+  }        
   return (
-   <div>
-     <h1>The Coins! {loading ? null : `(total: ${coins.length})`}</h1>
-     {loading ? <strong>Loading...</strong> :
+    <div>
+      <h2>My ToDos({todos.length})</h2>
       <form onSubmit={onSubmit}>
-      <div>
-        <input
-        onChange={onChange}
-        value = {myMoney}
-        type="text"
-        placeholder="write your money">
-        </input>
-        <button>Confirm</button>
-        <br></br>
-        {(money !== 0 && money != null && value !=="defaultValue" && value!== "") ? 
-        <h3>Available to buy coins (Selected Coin) : {money} ({value})</h3> : null}
-        <br></br>
-        <select onChange={onSelectChange}>
-          <option value="defaultValue">select the coin</option>
-        {coins.map((coin) => 
-            <option value = {coin.symbol} key={coin.id}>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
-        )}
-        </select>
-      </div>
+        <input onChange={onChange} type="text" placeholder="write toDo" value={todo}></input>
+        <button>submit</button>
       </form>
-    }
-   </div>
-  )
+      <ul>
+        {todos.map( (item, index) => <li
+        key={index}>{item}</li>)}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
